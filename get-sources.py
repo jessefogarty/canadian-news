142362291321bd90e9587e89123453ba6e1598e6
Jesse Fogarty <jesse@email.tg>
2018-12-14 00:48:01 -0500

    get-sources.py now uses a single (manually created) dictionary. Makes the for loop re.search through row (links) relatively "dynamic".

--- get-sources.py
+++ get-sources.py
@@ -7,20 +7,24 @@ class Sources():
     def __init__(self):
         self.conn = sqlite3.connect('testdb.db')
         self.cur = self.conn.cursor()
-        self.sources = {'cbc.ca':'CBC','thestar.com':'Toronto Star'}
+        self.sources = {'cbc.ca':'CBC','thestar.com':'Toronto Star','macleans.ca':'Macleans Magazine',
+                        'ottawacitizen.com':'Ottawa Citizen','montrealgazette.com':'Montreal Gazette',
+                        'vancouversun.com':'Vancouver Sun','financialpost.com':'Financial Post',
+                        'torontosun.com':'Toronto Sun','nationalpost.com':'National Post',
+                        'globalnews.ca':'Global News','ctvnews.ca':'CTV News',
+                        'edmontonjournal.com':'Edmonton Journal','vice.com':'VICE',
+                        'torontoist.com':'Torontoist','nationalobserver.com':'National Observer'}
     def find(self):
         st = time.time()
-        allrows = self.cur.execute('select id, link from articles').fetchall()
-        #for key, value in self.sources.items():
-            #search = key ; source = value
-            #print(search, source)
+        allrows = self.cur.execute('select id, link from articles where source is null').fetchall()
         for row in allrows:
             # Define id : row variables
             id = row[0] ; link = row[1]
+            # use dict to find / add source dynamically
             for key, value in self.sources.items():
                 search = key ; source = value
                 if re.search(search, link):
-                    print(link, source)
+                    self.cur.execute('update articles set source = ? where id = ?', (source, id))
             #if re.search('cbc.ca', link):
                 #source = 'CBC'
                 #self.cur.execute('update articles set source = ? where id = ?', (source, id))
