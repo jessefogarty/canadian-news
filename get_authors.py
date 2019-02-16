fd630bea7e02f5de01b10c990db78b9b7221af04
Jesse Fogarty <jesse@email.tg>
2019-02-16 13:18:44 -0500

    get authors v1
    Hacked version that runs a duplicate article download. The first in getcontent and the second in getauthor.
    
    v0.4 going to amalgamate the two methods. Also tailor feedparser
    
    Starting with a fresh database. Dropping tables was resulting in a high file size
    
    DB renamed as a secondary (cleaned) one is needed later on.

--- get_authors.py
+++ get_authors.py
@@ -1,28 +1,58 @@
-#!/usr/bin/python3
-from newspaper import Article
-import re
+#!/usr/bin/env python3
+'''
+    Browse article data from the database. Includes cleaning features.
+'''
 import sqlite3
-import time
 from tqdm import tqdm
+import time
+from newspaper import Article
+import spacy
+import re
+
+print('Loading Data Model: en_core_web_lg')
+st = time.time()
+#nlp = spacy.load('en_core_web_lg', disable=['tagger', 'parser'])
 
 conn = sqlite3.connect('testdb.db')
 cur = conn.cursor()
 
-db = cur.execute('select id, link from articles limit 300').fetchall()
+links = cur.execute('''select id,link from articles where
+ author is null limit 5''').fetchall()
+print('test', cur.execute('select id from articles order by id DESC limit 1').fetchall()[0][0])
+total = len(links)
+count = 0
 
-for row in tqdm(db):
-    id = row[0] ; link = row[1]
-    content = Article(link)
-    content.download()
+for link in tqdm(links):
+    id = link[0]
+    url = link[1]
+    # Create Article opbject w/ url & .download()
+    article = Article(url)
     try:
-        content.parse()
-        try:
-            authors = content.authors
-            print(authors)
-        except:
-            authors = None
+        # catch article download errors (404 etc)
+        article.download()
+        # Parse article object
+        article.parse()
+        authors = article.authors
+        authors_rev = []
+        # Clean newspaper authors w/ spaCy NER
+        for author in authors:
+            doc = nlp(author)
+            for ent in doc.ents:
+                # print(ent.text, ent.label_)
+                if ent.label_ == 'PERSON':
+                    if ent.text not in authors_rev:
+                        authors_rev.append(ent.text)
+        # Only submit DB changes if revised author list contains 1+ name(s)
+        if len(authors_rev) > 0:
+            string = ", ".join(authors_rev)
+            updata = (string, id)
+            cur.execute('''UPDATE articles SET author = ? WHERE id = ? ''', (updata))
+            count += 1
     except:
+        #TODO: Add print to log file - error downloading row = id url = link
         pass
-ft = time.time() - st ; print('finished in:', ft, 'seconds.')
-
-#cur.execute('update articles set ')
+ft = time.time() - st
+#TODO: ADD print to log file - to compare run time / errors
+print(f'finished updating {count} / {total} rows in {ft} seconds.')
+conn.commit()
+conn.close()
